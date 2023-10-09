71b4033d7db21d0b0b74a7c2cc67f02c2606c188
6I3L <145340475+6I3L@users.noreply.github.com>
2023-10-09 11:34:53 +0200

    l

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
-<script>
-
-</script>
-
 <template>
-  <h1>Welkom in de Vue App</h1>
+  <div id="app">
+    <input v-model="product" type="text" placeholder="Voeg een product toe" />
+    <button @click="addProduct">Toevoegen</button>
+    <ul>
+      <li v-for="item in products" :key="item.id">{{ item.name }}</li>
+      <li></li> <!-- Leeg list item zoals gevraagd -->
+    </ul>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      product: "", // Inputveld gelinkt aan variabele product
+      products: [], // Lege array voor producten
+    };
+  },
+  methods: {
+    addProduct() {
+      if (this.product.trim() !== "") {
+        this.products.push({ id: Date.now(), name: this.product });
+        this.product = ""; // Reset het inputveld na toevoegen
+      }
+    },
+  },
+};
+</script>
+
 <style>
+/* CSS-regels uit het gegenereerde css-bestand */
+/* Voeg hier de css-regels toe die je uit het css-bestand hebt gekopieerd */
 </style>
